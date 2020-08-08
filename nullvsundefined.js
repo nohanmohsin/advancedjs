19ed85cc2c37f33a804d4b39b4e87dd7d914305b
nohanmohsin <67507872+nohanmohsin@users.noreply.github.com>
2020-08-08 20:37:10 +0600

    nullvsundefined done

---
+++ nullvsundefined.js
@@ -0,0 +1,27 @@
+// function add(num1, num2) {
+    // console.log(num1 + num2);
+// }
+//value of result is undefined
+// const result = add(10, 30);
+// console.log(result);
+
+
+// if all params are not given then that params value is undefined 
+// function add(num1, num2) {
+    // console.log(num1, num2);
+// }
+//value of result is undefined
+// const result = add(10);
+// console.log(result);
+
+
+
+// if object property name is incorrect
+// const person = {name: "name", phone: "010204"}
+// console.log(person.mothername);
+
+// if value is undefined (duh)
+// let fun = undefined;
+// console.log(fun);
+
+//finally null is just a placeholder value
