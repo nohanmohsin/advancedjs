25e1049607d05789aa7150a8ef209acac970b8cd
nohanmohsin <67507872+nohanmohsin@users.noreply.github.com>
2020-08-08 20:16:43 +0600

    truthyfalsy done

---
+++ truthyFalsy.js
@@ -0,0 +1,18 @@
+//falsy
+//0
+//""
+//undefined
+//null
+//NaN
+
+//truthy
+//"0"" " [] =>empty data type
+let name;
+
+if (name) {
+    console.log("true");
+}
+
+else{
+    console.log("false");
+}
